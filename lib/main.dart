89d5caf1541ad1732041f7e5f4eeb2aa82ac588a
corebootgit <coreboot@tlen.pl>
2020-05-24 22:10:08 +0200

    testpush

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Yeelight Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
